f408aa2a26fc8886951e5d401a8056644fa0db2e
nguyenhoangtuanvu <55772977+nguyenhoangtuanvu@users.noreply.github.com>
2022-01-20 22:25:09 +0700

    master2 commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -73,3 +73,57 @@ function contentHome() {
     content_detail.classList.remove('active')
 }
 sidebarBtn_home.addEventListener('click',contentHome)
+
+
+// poftfolio overlay
+
+    // open/close model
+function openPortfolioOverlay() {
+    document.getElementById('model').style.display = "block";
+}
+function closePortfolioOverlay() {
+    document.getElementById('model').style.display = "none";
+}
+
+var getOpenModelBtn = document.querySelectorAll('.project-thumbnail');
+var getCloseModelBtn = document.querySelector('.close');
+for(var btn of getOpenModelBtn) {
+    btn.addEventListener('click',openPortfolioOverlay);
+}
+getCloseModelBtn.addEventListener('click',closePortfolioOverlay);
+
+// model content
+var imgIndex = 1;
+showImg(imgIndex);
+
+function plussImg(n) {
+    showImg(imgIndex += n);
+}
+function currentProject(n) {
+    showImg(imgIndex = n);
+}
+function showImg(n) {
+    var i;
+    var project = document.getElementsByClassName('poftfolio__img-main');
+    var imgDemo = document.getElementsByClassName('project-detail__img-demo');
+    
+    if(imgIndex > project.length) {imgIndex = 1}
+    if(imgIndex < 1) {imgIndex = project.length}
+    for(i = 0; i< project.length; i++) {
+        project[i].style.display = "none";
+    }
+    // for(i = 0; i< imgDemo.length; i++) {
+    //     imgDemo[i].className = imgDemo[i].className.replace(" active", "");
+    // }
+    project[imgIndex-1].style.display = "block";
+    imgDemo[imgIndex-1].className += " imgDemo-active";
+}
+
+// google map
+// function googleMap() {
+//     var mapPop = {
+//         center:new google.maps.LatLng(51.508742,-0.120850),
+//         zoome:5,
+//     };
+//     var map = new google.maps.Map(document.getElementById('google-map'),mapPop)
+// }
